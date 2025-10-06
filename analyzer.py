d09296d6a73cede310d8cde78f08a04fe938b7c5
Unixxxxxx <sudhanshuroyss208@gmail.com>
2025-10-06 11:50:19 +0530

    &*__

--- analyzer.py
+++ analyzer.py
@@ -13,3 +13,16 @@ data['Total Sales'] = data['Units Sold'] * data['Unit Price']
 
 print("Cleaned Data With Totle Sales:")
 print(data.head())
+
+#Analyzer data
+
+summary = {
+    "Total Sales (₹)": [data['Total Sales'].sum()],
+    "Average Sale per Region (₹)": [data.groupby('Region')['Total Sales'].mean().mean()],
+    "Top Selling Product": [data.groupby('Product')['Total Sales'].sum().idxmax()],
+    "Best Region": [data.groupby('Region')['Total Sales'].sum().idxmax()]
+}
+
+summary_df = pd.DataFrame(summary)
+print("Sales Summary:")
+print(summary_df)
