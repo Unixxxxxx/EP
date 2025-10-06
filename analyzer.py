70f2d5fffde7e2b64d29950c86a0cc0ade17902b
Unixxxxxx <sudhanshuroyss208@gmail.com>
2025-10-06 07:57:18 +0530

    &&__8

--- analyzer.py
+++ analyzer.py
@@ -1,6 +1,15 @@
 import pandas as pd
 
+#Load Excel data
 data = pd.read_excel("sales_data.xlsx")
 
 print("Original Data")
+print(data.head())
+
+
+#Clean Data
+data.dropna(inplace=True)
+data['Total Sales'] = data['Units Sold'] * data['Unit Price']
+
+print("Cleaned Data With Totle Sales:")
 print(data.head())
